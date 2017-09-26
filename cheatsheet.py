c50525710d025cb85e9c6d388e20f3b07a32a925
Alessandro Valli <av@oblong.com>
2017-09-26 18:59:49 +0200

    worked on the cheat sheet

--- cheatsheet.py
+++ cheatsheet.py
@@ -1,3 +1,5 @@
+from __future__ import print_function  # per potere usare la print () con end='' di Python 3
+
 # import is used to make specialty functions available
 # These are called modules
 import random
@@ -16,6 +18,9 @@ This is a multi-line comment
 # Its name is like a label for that value
 name = "Derek"
 print(name)
+
+num = 37.3
+print (num)
 
 # A variable name can contain letters, numbers, or _
 # but can't start with a number
@@ -44,7 +49,7 @@ print("(1 + 2 - 3) * 2 =", (1 + 2 - 3) * 2)
 
 # A string is a string of characters surrounded by " or '
 # If you must use a " or ' between the same quote escape it with \
-quote = "\"Always remember your unique,"
+quote = "\"Always remember you are unique,"
 
 # A multi-line quote
 multi_line_quote = ''' just
@@ -57,6 +62,7 @@ print("%s %s %s" % ('I like the quote', quote, multi_line_quote))
 
 # To keep from printing newlines use end=""
 # print("I don't like ",end="") # non gli piace a Python 2.7
+print("I don't like ",end="") # grazie a import future in cima al file, usiamo la print () di Python 3
 print("newlines")
 
 # You can print a string multiple times with *
@@ -66,16 +72,16 @@ print('\n' * 5)
 # A list allows you to create a list of values and manipulate them
 # Each value has an index with the first one starting at 0
 
-grocery_list = ['Juice', 'Tomatoes', 'Potatoes', 'Bananas']
-print('The first item is', grocery_list[1])
+grocery_list = ['Juice', 'Tomatoes', 'Potatoes', "Bananas"]
+print('The first item is', grocery_list[0])  # ecco
 
 # You can change the value stored in a list box
 grocery_list[0] = "Green Juice"
 print(grocery_list)
 
-# You can get a subset of the list with [min:up to but not including max]
+# You can get a subset of the list with <min>:<up to but not including max>
 
-print(grocery_list[1:3])
+print(grocery_list[1:3]) # due a partire dal secondo compreso
 
 # You can put any data type in a a list including a list
 other_events = ['Wash Car', 'Pick up Kids', 'Cash Check']
@@ -83,15 +89,17 @@ to_do_list = [other_events, grocery_list]
 
 print(to_do_list)
 
-# Get the second item in the second list (Boxes inside of boxes)
-print(to_do_list[1][1])
+# Get the third item in the second list (Boxes inside of boxes)
+print(to_do_list[1][2])
 
 # You add values using append
 grocery_list.append('onions')
 print(to_do_list)
 
 # Insert item at given index
-grocery_list.insert(1, "Pickle")
+grocery_list.insert (1, "Pickle") # in seconda posizione
+
+print (grocery_list)
 
 # Remove item from list
 grocery_list.remove("Pickle")
@@ -102,9 +110,11 @@ grocery_list.sort()
 # Reverse sort items in list
 grocery_list.reverse()
 
+print(grocery_list)
+print ("deleting")
 # del deletes an item at specified index
 del grocery_list[4]
-print(to_do_list)
+print(grocery_list)
 
 # We can combine lists with a +
 to_do_list = other_events + grocery_list
@@ -124,20 +134,28 @@ print(min(to_do_list))
 
 pi_tuple = (3, 1, 4, 1, 5, 9)
 
+
+print (pi_tuple)
+
 # Convert tuple into a list
-new_tuple = list(pi_tuple)
+new_list = list(pi_tuple)
+print (new_list)
 
 # Convert a list into a tuple
-# new_list = tuple(grocery_list)
+new_tuple = tuple(grocery_list)
+print (new_tuple)
 
-# tuples also have len(tuple), min(tuple) and max(tuple)
+
+print (len(pi_tuple))
+print (min(pi_tuple))
+print (max(pi_tuple))
 
 # DICTIONARY or MAP -------------
 # Made up of values with a unique key for each value
 # Similar to lists, but you can't join dicts with a +
 
 super_villains = {'Fiddler' : 'Isaac Bowin',
-                  'Captain Cold' : 'Leonard Snart',
+                  'Captain Cold' : "Leonard Snart",
                   'Weather Wizard' : 'Mark Mardon',
                   'Mirror Master' : 'Sam Scudder',
                   'Pied Piper' : 'Thomas Peterson'}
@@ -174,7 +192,7 @@ print(super_villains.values())
 
 age = 30
 if age > 16 :
-    print('You are old enough to drive')
+  print('You are old enough to drive')  # indentazione di minimo 1 spazio
 
 # Use an if statement if you want to execute different code regardless
 # of whether the condition ws met or not
@@ -210,9 +228,9 @@ else:
 # Allows you to perform an action a set number of times
 # Range performs the action 10 times 0 - 9
 for x in range(0, 10):
-    print(x , ' ') # non gli piace , end="")
+    print(x , ' ', end="")
 
-print('\n')
+print('')
 
 # You can use for loops to cycle through a list
 grocery_list = ['Juice', 'Tomatoes', 'Potatoes', 'Bananas']
@@ -220,9 +238,13 @@ grocery_list = ['Juice', 'Tomatoes', 'Potatoes', 'Bananas']
 for y in grocery_list:
     print(y)
 
+
+
 # You can also define a list of numbers to cycle through
 for x in [2,4,6,8,10]:
     print(x)
+print ()
+
 
 # You can double up for loops to cycle through lists
 num_list =[[1,2,3],[10,20,30],[100,200,300]];
@@ -234,41 +256,37 @@ for x in range(0,3):
 # WHILE LOOPS -------------
 # While loops are used when you don't know ahead of time how many
 # times you'll have to loop
-random_num = random.randrange(0,100)
+random_num = random.randrange(0,100) # tra zero e 99 credo
 
+print ()
 while (random_num != 15):
     print(random_num)
     random_num = random.randrange(0,100)
+print ()
 
 # An iterator for a while loop is defined before the loop
 i = 0;
 while (i <= 20):
-    if(i%2 == 0):
+    if(i % 2 == 0):
         print(i)
-    elif(i == 9):
+    elif (i == 9):
         # Forces the loop to end all together
         break
     else:
-        # Shorthand for i = i + 1
-        i += 1
+        i += 1  # for odd numbers
         # Skips to the next iteration of the loop
         continue
+    i += 1  # for even numbers
 
-    i += 1
+print ()
 
 # FUNCTIONS -------------
-# Functions allow you to reuse and write readable code
-# Type def (define), function name and parameters it receives
-# return is used to return something to the caller of the function
 def addNumbers(fNum, sNum):
     sumNum = fNum + sNum
     return sumNum
 
 print(addNumbers(1, 4))
 
-# Can't get the value of rNum because it was created in a function
-# It is said to be out of scope
-# print(sumNum)
 
 # If you define a variable outside of the function it works every place
 newNum = 0;
@@ -276,22 +294,26 @@ def subNumbers(fNum, sNum):
     newNum = fNum - sNum
     return newNum
 
-print(subNumbers(1, 4))
+subNumbers(1, 4)
+print(newNum) # interesting
+newNum = subNumbers(1, 4)
+print(newNum)
 
 # USER INPUT -------------
 print('What is your name?')
 
 # Stores everything typed up until ENTER
-name = sys.stdin.readline()
+# name = sys.stdin.readline()
+name = 'Alex'
 
 print('Hello', name)
 
 # STRINGS -------------
 # A string is a series of characters surrounded by ' or "
-long_string = "I'll catch you if you fall - The Floor"
+long_string = "i'll catch you if you fall - The Floor"
 
-# Retrieve the first 4 characters
-print(long_string[0:4])
+# Retrieve the first 6 characters
+print(long_string[0:6])
 
 # Get the last 5 characters
 print(long_string[-5:])
@@ -304,15 +326,18 @@ print(long_string[:4] + " be there")
 
 # String formatting
 print("%c is my %s letter and my number %d number is %.5f" % ('X', 'favorite', 1, .14))
+print ("%s is %d" % ("Ale", 42))
 
 # Capitalizes the first letter
-print(long_string.capitalize())
+long_string.capitalize()
+print(long_string)  # interesting, Python strings are immutable
+print(long_string.capitalize ())
 
 # Returns the index of the start of the string
 # case sensitive
 print(long_string.find("Floor"))
 
-# Returns true if all characters are letters ' isn't a letter
+# Returns true if all characters are letters... ' isn't a letter
 print(long_string.isalpha())
 
 # Returns true if all characters are numbers
@@ -359,12 +384,22 @@ print(text_in_file)
 # Delete the file
 os.remove("test.txt")
 
+def factorial (number) :
+  if (number > 1) :
+    return number * factorial (number - 1)
+  return 1
+
+for i in range (0, 7) :
+  print (i, "    ", factorial (i))
+
+print ()
+
 # CLASSES AND OBJECTS -------------
 # The concept of OOP allows us to model real world things using code
 # Every object has attributes (color, height, weight) which are object variables
 # Every object has abilities (walk, talk, eat) which are object functions
 
-class Animal(object):
+class Animal (object) :
     # None signifies the lack of a value
     # You can make a variable private by starting it with __
     __name = None
@@ -423,10 +458,11 @@ print(cat.toString())
 
 class Dog(Animal):
     __owner = None
+    __animal_type = "Dog"
 
     def __init__(self, name, height, weight, sound, owner):
         self.__owner = owner
-        self.__animal_type = None
+        # self.__animal_type = "Dog" # interesting, can be omitted
 
         # How to call the super class constructor
         super(Dog, self).__init__(name, height, weight, sound)
@@ -438,7 +474,7 @@ class Dog(Animal):
         return self.__owner
 
     def get_type(self):
-        print ("Dog")
+        print (self.__animal_type)
 
     # We can overwrite functions in the super class
     def toString(self):
@@ -448,7 +484,7 @@ class Dog(Animal):
     # This allows for method overloading
     def multiple_sounds(self, how_many=None):
         if how_many is None:
-            print(self.get_sound)
+            print(self.get_sound())
         else:
             print(self.get_sound() * how_many)
 
@@ -469,3 +505,8 @@ test_animals.get_type(cat)
 test_animals.get_type(spot)
 
 spot.multiple_sounds(4)
+spot.multiple_sounds(0)
+spot.multiple_sounds(-1)
+spot.multiple_sounds()
+
+
