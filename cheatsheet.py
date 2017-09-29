3e7bd88953aae7ff18c74054734c72e348b25933
Alessandro Valli <av@oblong.com>
2017-09-29 15:57:06 +0200

    boto upload to bucket

--- cheatsheet.py
+++ cheatsheet.py
@@ -608,3 +608,38 @@ interrogate(Dog)
 interrogate(spot)
 interrogate(addNumbers)
 
+
+AWS_ACCESS_KEY_ID = os.environ['AWSAccessKeyId']
+AWS_SECRET_ACCESS_KEY = os.environ['AWSSecretKey']
+
+import boto
+import boto.s3
+from boto.s3.key import Key
+
+bucket_name = 'natural-interaction'
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY)
+
+location='EU'
+print (location)
+
+#bucket = conn.create_bucket(bucket_name, location = 'EU')
+bucket = conn.get_bucket(bucket_name)
+
+testfile = "samples/opencv-logo.png"
+print (testfile)
+print (bucket_name)
+
+
+def percent_cb(complete, total):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+
+k = Key(bucket)
+k.key = 'test3.png'
+k.set_contents_from_filename(testfile,
+    cb=percent_cb, num_cb=10)
+
+
+
