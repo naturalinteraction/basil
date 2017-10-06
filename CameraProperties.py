a6e489794c63418972336b7f8dd853596647a97a
Alessandro Valli <av@naturalinteraction.org>
2017-10-06 12:31:31 +0200

    CameraProperties in cap

--- CameraProperties.py
+++ CameraProperties.py
@@ -44,51 +44,30 @@ class CameraProperties(object):
         name = self.CurrentPropertyName()
         if self.values_indices[name] < (len(self.properties[name]) - 1):
             self.values_indices[name] += 1
+        self.PrintCurrentProperty()
 
     def DecValue(self):
         name = self.CurrentPropertyName()
         if self.values_indices[name] > 0:
             self.values_indices[name] -= 1
+        self.PrintCurrentProperty()
 
     def IncProperty(self):
         if self.property_index < (len(self.properties) - 1):
             self.property_index += 1
+        self.PrintCurrentProperty()
 
     def DecProperty(self):
         if self.property_index > 0:
             self.property_index -= 1
+        self.PrintCurrentProperty()
 
     def Save(self):
         with open('camera-properties.pkl', 'wb') as f:
             pickle.dump(self.values_indices, f, 0)
+        print('Saved.')
 
     def Load(self):
         with open('camera-properties.pkl', 'rb') as f:
             self.values_indices = pickle.load(f)
-
-if True:
-    cp = CameraProperties ()
-    print(cp.values_indices) 
-    cp.PrintCurrentProperty()
-    #cp.Load()
-    print(cp.values_indices) 
-    cp.IncProperty()
-    cp.PrintCurrentProperty()
-    cp.IncProperty()
-    cp.PrintCurrentProperty()
-    for n in range(0, 100):
-        cp.IncValue()
-    cp.IncProperty()
-    cp.PrintCurrentProperty()
-    cp.DecValue()
-    cp.PrintCurrentProperty()
-    cp.DecProperty()
-    cp.PrintCurrentProperty()
-    cp.IncValue()
-    cp.PrintCurrentProperty()
-    print(cp.values_indices) 
-    cp.Save()
-    cp.PrintCurrentProperty()
-    print(cp.values_indices) 
-    print(cp.values_indices) 
-    cp.PrintAllProperties()
+        print('Loaded.')
--- cap.py
+++ cap.py
@@ -2,12 +2,13 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+from CameraProperties import CameraProperties
 
 def PrintFullCameraState():
     print("-----------------------------------------------------------")
-    print("ISO", camera.iso)  # With iso settings other than 0 (auto), the exposure_mode property becomes non-functional.
+    print("ISO", int(camera.iso))  # With iso settings other than 0 (auto), the exposure_mode property becomes non-functional.
     print("AWB mode", camera.awb_mode)  # The default is auto
-    print("AWB gains", camera.awb_gains) # with awbmode off, (red, blue) tuple  Typical values for the gains are between 0.9 and 1.9
+    print("AWB gains", float(camera.awb_gains[0]), float(camera.awb_gains[1])) # with awbmode off, (red, blue) tuple  Typical values for the gains are between 0.9 and 1.9
     print("brightness", camera.brightness)
     print("constrast", camera.contrast)
     print("saturation", camera.saturation)
@@ -15,14 +16,15 @@ def PrintFullCameraState():
     print("exposure mode", camera.exposure_mode) # The default is auto
     print("exposure compensation", camera.exposure_compensation)
     print("meter mode", camera.meter_mode)  # for exposure
-    print("shutter speed", camera.shutter_speed)  # microseconds, or 0 which indicates that the speed will be automatically determined by the auto-exposure algorithm
+    print("shutter speed", int(camera.shutter_speed))  # microseconds, or 0 which indicates that the speed will be automatically determined by the auto-exposure algorithm
     print("DRC strength", camera.drc_strength) # off, low, medium, high
     # still not in the menu:
     print("zoom", camera.zoom)  # (x, y, w, h) not interesting at the moment
-    print('exposure speed', camera.exposure_speed) # READONLY
-    print("analog gain", camera.analog_gain) # READONLY
-    print("digital gain", camera.digital_gain) # READONLY
-
+    print('exposure speed', int(camera.exposure_speed)) # READONLY
+    print("analog gain", float(camera.analog_gain)) # READONLY
+    print("digital gain", float(camera.digital_gain)) # READONLY
+    print("-----------------------------------------------------------")
+    
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (1280, 1024)
@@ -36,14 +38,14 @@ time.sleep(1)
 #camera.contrast = 50  
 #camera.saturation = 90 
 #camera.sharpness = 30
-camera.awb_mode = 'off'
-camera.awb_gains = (1.0, 1.0)
-camera.iso = 800
+camera.awb_mode = 'auto'
+#camera.awb_gains = (1.0, 1.0)
+#camera.iso = 800
 camera.exposure_mode = 'auto'
 camera.exposure_compensation = 25
-camera.shutter_speed = 54510L
+#camera.shutter_speed = 54510L
+camera.drc_strength = 'high'
 '''
-camera = PiCamera(resolution=(1280, 720), framerate=30)
 # Set ISO to the desired value
 camera.iso = 100
 # Wait for the automatic gain control to settle
@@ -55,15 +57,12 @@ g = camera.awb_gains
 camera.awb_mode = 'off'
 camera.awb_gains = g
 # Finally, take several photos with the fixed settings
-camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])
-
 '''
 
 show = True
 
-valori_iso = [100, 200, 320, 400, 500, 640, 800]
-indice_iso = 0
-print(len(valori_iso))
+cp = CameraProperties ()
+cp.Load()
 
 # capture frames from the camera
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -77,31 +76,39 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         # show = False
         key = cv2.waitKey(1) & 0xFF
         
-        PrintFullCameraState()
         
-        #if key < 255: print (key)
-        if key == 82: print ("up")
-        if key == 84: 
-            print ("down")
-            camera.drc_strength = 'high'
-            camera.shutter_speed = 59000
-        if key == 81: 
-            print ("left")
-            if indice_iso > 0:
-              indice_iso-=1
-            print (valori_iso[indice_iso])
-            camera.iso = valori_iso[indice_iso]
-        if key == 83:
-            print ("right")
-            if indice_iso < len(valori_iso)-1:
-              indice_iso+=1
-            print (valori_iso[indice_iso])
-            camera.iso = valori_iso[indice_iso]
+        
+        # if key < 255: print (key)
+        
+        if key == ord('s'):
+          cp.Save()
+    
+        if key == ord('c'):
+          cp.PrintCurrentProperty()
+    
+        if key == ord('a'):
+          cp.PrintAllProperties()
+    
+        if key == ord('u'):
+          PrintFullCameraState()
+          
+        if key == 82:  # up
+            cp.DecProperty()
+        
+        if key == 84: # down
+            cp.IncProperty()
+            
+        if key == 81: # left
+            cp.DecValue()
+            
+        if key == 83:  # right
+            cp.IncValue()
+ 
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
  
 	# if the `q` or ESC key was pressed, break from the loop
-	if key == ord("q") or key == 27:
+	if key == ord('q') or key == 27:
 		break
 camera.close()
-print('camera closed')
+print('Camera closed.')
