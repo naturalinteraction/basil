eb04cb148fbb30a95580e644ab4c46ddc8cf55f4
Alessandro Valli <av@naturalinteraction.org>
2017-10-12 11:07:38 +0200

    integrate audio volume measurements

--- audio.py
+++ audio.py
@@ -7,14 +7,16 @@ inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
 
 inp.setchannels(1)
 inp.setrate(8000)  # 16000
-inp.setformat(alsaaudio.PCM_FORMAT_U8)  # PCM_FORMAT_S16_LE for ubuntu
+inp.setformat(alsaaudio.PCM_FORMAT_U8)  # PCM_FORMAT_S16_LE for laptop
 inp.setperiodsize(160)
 
 while True:
-    # Read data from device
-    l,data = inp.read()
-    if l:
-        # Return the maximum of the absolute value of all samples in a fragment.
-        volume = audioop.max(data, 1) # 2 for ubuntu
-        print(volume)
-    time.sleep(.001)
+    volume = 0.0
+    for i in range(0, 50):
+        # Read data from device
+        l,data = inp.read()
+        if l:
+            # Return the maximum of the absolute value of all samples in a fragment.
+            volume += audioop.max(data, 1) # 2 for laptop
+        time.sleep(0.002)
+    print(int(volume * volume / 1000.0 / 1000.0 / 2))
