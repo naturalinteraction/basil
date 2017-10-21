e77d49e905e6e0ddd3812ff61e15b8d192c04e15
Alessandro Valli <av@naturalinteraction.org>
2017-10-21 10:01:15 +0200

    raspistill in autostart script

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,2 @@
 cd /home/pi/basil
-xterm -hold -e "source ~/set_basil_vars.sh ; pwd ; sleep 30 ; echo $BASIL_NOTE ; python cap.py"
+xterm -hold -e "source ~/set_basil_vars.sh ; pwd ; echo 'raspistill...' ; raspistill -o ~/autostart.jpg ; echo 'waiting...' ; sleep 30 ; echo $BASIL_NOTE ; python cap.py"
