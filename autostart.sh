15be0f07790bb8b90b782f050b0f4649eff714b2
Alessandro Valli <av@naturalinteraction.org>
2017-10-21 10:52:05 +0200

    more juggling in autostart script

--- autostart.sh
+++ autostart.sh
@@ -1,2 +1,9 @@
+source ~/set_basil_vars.sh
+echo 'raspistill...'
+raspistill -o ~/autostart.jpg
+echo 'waiting...'
+sleep 30
+echo $BASIL_NOTE
 cd /home/pi/basil
-xterm -hold -e "source ~/set_basil_vars.sh ; pwd ; echo 'raspistill...' ; raspistill -o ~/autostart.jpg ; echo 'waiting...' ; sleep 30 ; echo $BASIL_NOTE ; python cap.py"
+pwd
+xterm -hold -e "source ~/set_basil_vars.sh ; pwd ; echo 'raspistill...' ; raspistill -o ~/autostart.jpg ; echo 'waiting more...' ; sleep 30 ; echo $BASIL_NOTE ; python cap.py"
