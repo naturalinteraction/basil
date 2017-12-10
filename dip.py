971afc302a977ea2276493f5aa71f4c5c6c63772
Alessandro Valli <av@oblong.com>
2017-12-10 15:54:17 +0100

    testing findContours / fillPoly

--- dip.py
+++ dip.py
@@ -11,8 +11,8 @@ import shutil
 import pickle
 import os.path
 import numpy as np
-from segment import segment 
-    
+from segment import segment
+
 git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip()
 print(git_hash)
 git_commit_message = subprocess.check_output(["git", "log", "-1"]).strip()  # , "--pretty=%B"
@@ -56,20 +56,35 @@ for f in sorted(downloaded_files):
     if True:  # average[0] > 30:
         # hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         before = time.time()
-        count = segment(image, -20, 10, -10, -900, 70)
-        print('count ' + str(count))
-        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image_copy = image.copy()
+        count = segment(image_copy, -20, 10, -10, -1200, 70)
+        # print('count ' + str(count))
+        gray_image = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
 
         kernel = np.ones((3, 3), np.uint8)
         gray_image = cv2.erode(gray_image, kernel, iterations = 1)
         # gray_image = cv2.dilate(gray_image, kernel, iterations = 2)
         # gray_image = cv2.morphologyEx(gray_image, cv2.MORPH_OPEN, kernel)
         # gray_image = cv2.morphologyEx(gray_image, cv2.MORPH_CLOSE, kernel)
+
+        gray_image = cv2.bitwise_not(gray_image)
+
+        im2, contours, heirarchy = cv2.findContours(gray_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        for cnt in contours:
+            m = cv2.moments(cnt)
+            area = m['m00']
+            if area <= 4000:
+                cv2.fillPoly(gray_image, pts = [cnt], color=(0))
+
         count = len(cv2.findNonZero(gray_image))
-        print('count after erosion ' + str(count))
+        print('count after fill ' + str(count))
+
+        mask = cv2.cvtColor(gray_image, cv2.COLOR_GRAY2BGR)
+
+        image = cv2.addWeighted(image, 1.0, mask, 0.5, 0.0)
 
         print(time.time() - before)
-        cv2.imshow('dip', gray_image)
+        cv2.imshow('dip', image)
 
         filename = f.replace('downloaded/', 'temp/')
         cv2.imwrite(filename + '.jpeg', image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
