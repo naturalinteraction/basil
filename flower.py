ec824a6bd9583e05a2a738e6d9ec1564e94184df
Alessandro Valli <av@naturalinteraction.org>
2017-12-23 12:40:42 +0100

    reverting flower to not detecting exceptions ; cleaned version string

--- flower.py
+++ flower.py
@@ -18,29 +18,22 @@
 import sys
 import string
 from gattlib import GATTRequester, GATTResponse
-# from struct import *
+from struct import *
 
 address = sys.argv[1]
 requester = GATTRequester(address)
 # Read battery and firmware version attribute
-try:
-    data = requester.read_by_handle(0x0038)[0]
-    battery, version = unpack('<B6s',data)
-    # version = filter(version.isalnum, string.printable)
-    print "Battery level:",battery,"%"
-    print "Firmware version:",version
-except:
-    print("battery and firmware read failed")
-
-try:
-    # Enable real-time data reading
-    requester.write_by_handle(0x0033, str(bytearray([0xa0, 0x1f])))
-    # Read plant data
-    data = requester.read_by_handle(0x0035)[0]
-    temperature, sunlight, moisture, fertility = unpack('<hxIBHxxxxxx',data)
-    print "Light intensity:",sunlight,"lux (> 1000 lux)"
-    print "Temperature:",temperature/10.,"C (17C - 26C)"
-    print "Soil moisture:",moisture,"% (35% - 90%)"
-    print "Soil fertility:",fertility,"uS/cm (200 uS/cm - 1200 uS/cm)"
-except:
-    print("sensor read failed")
+data = requester.read_by_handle(0x0038)[0]
+battery, version = unpack('<B6s',data)
+version = filter(lambda x : x in string.printable, version)
+print "Battery level:",battery,"%"
+print "Firmware version:",version
+# Enable real-time data reading
+requester.write_by_handle(0x0033, str(bytearray([0xa0, 0x1f])))
+# Read plant data
+data = requester.read_by_handle(0x0035)[0]
+temperature, sunlight, moisture, fertility = unpack('<hxIBHxxxxxx',data)
+print "Light intensity:",sunlight,"lux (> 1000 lux)"
+print "Temperature:",temperature/10.,"C (17C - 26C)"
+print "Soil moisture:",moisture,"% (35% - 90%)"
+print "Soil fertility:",fertility,"uS/cm (200 uS/cm - 1200 uS/cm)"
