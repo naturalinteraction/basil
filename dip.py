1e31a9e61e7bff65183d2dfaf580d795e804706f
Alessandro Valli <av@oblong.com>
2018-01-09 12:53:45 +0100

    FillHoles()

--- dip.py
+++ dip.py
@@ -33,57 +33,7 @@ for image_file in ListLocalImages('downloaded/' + args.prefix, args.substring):
 
     UpdateWindow('biomass_mask NOT FILLED', biomass_mask)  # this is still the raw mask, without the holes filled
 
-    # -------------------- HOLES BEGIN
-
-    # inverted mask, so that we can analyze the holes as white blobs against a black background
-    ignored1, contours, ignored2 = cv2.findContours(Inverted(biomass_mask), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    holes = 0
-    accepted_holes_mask = np.zeros(bgr.shape[:2], np.uint8)
-    refused_holes_mask = np.zeros(bgr.shape[:2], np.uint8)
-
-    ellipses = list()
-    circles = list()
-
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-
-        ### print(ContourStats(cnt))
-
-        if area < 70 * 70:
-            hole_mask = np.zeros(bgr.shape[:2], np.uint8)
-            cv2.drawContours(hole_mask, [cnt], -1, 255, -1)
-            mean = cv2.mean(hsv, mask=hole_mask)[0:3]
-
-            target_color_0 = 36
-            target_color_1 = 238
-            target_color_2 = 164
-
-            weight_color_0 = 6
-            weight_color_1 = 3
-            weight_color_2 = 1
-
-            color_distance = pow(mean[0] - target_color_0, 2) * weight_color_0 +  \
-                             pow(mean[1] - target_color_1, 2) * weight_color_1 +  \
-                             pow(mean[2] - target_color_2, 2) * weight_color_2
-
-            segmentation_threshold_holes = (90 * 90 * 3) * 1.7
-
-            if color_distance < segmentation_threshold_holes:
-                cv2.fillPoly(biomass_mask, pts = [cnt], color=(255))
-                cv2.fillPoly(accepted_holes_mask, pts = [cnt], color=(255))
-                # print(str(holes) + " area " + str(area) + ' dist ' + str(color_distance) + ' mean ' + str(mean))
-                holes += 1
-                if area > 5 * 5:
-                    if len(cnt) > 4:
-                        ellipses.append(cv2.fitEllipse(cnt))
-                    else:
-                        circles.append(cv2.minEnclosingCircle(cnt))
-            else:
-                cv2.fillPoly(refused_holes_mask, pts = [cnt], color=(255))
-
-    # print('holes ' + str(holes))
-
-    # -------------------- HOLES END
+    accepted_holes_mask,refused_holes_mask,ellipses,circles = FillHoles(biomass_mask, bgr, hsv)
 
     # this is done after because it needs the updated biomass_mask
     foreground = MaskedImage(bgr, biomass_mask)
@@ -97,6 +47,7 @@ for image_file in ListLocalImages('downloaded/' + args.prefix, args.substring):
     DrawEllipses(foreground, ellipses, white)
     DrawCircles(foreground, circles, white)
 
+    # todo: function
     nonzero = cv2.findNonZero(biomass_mask)
     if not(nonzero is None):
         count = len(nonzero)
