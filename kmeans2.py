96f4db88047fdacf1b530617dc9d67ebf4abad9f
Alessandro Valli <av@oblong.com>
2018-01-13 17:47:57 +0100

    first two tests

---
+++ kmeans2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('downloaded/test-test_2560x1920_2000_01_01-00_00.jpg')
+img = cv2.resize(img, (256 * 3, 192 * 3))
+Z = img.reshape((-1,3))
+
+# convert to np.float32
+Z = np.float32(Z)
+
+# define criteria, number of clusters(K) and apply kmeans()
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 8  # 4 6 8
+ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+# Now convert back into uint8, and make original image
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+
+cv2.imshow('res2',res2)
+cv2.imshow('orig',img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
