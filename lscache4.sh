77e4fceb1bf01ba1be6804f147761ec24c857661
Alessandro Valli <av@oblong.com>
2018-01-15 16:44:34 +0100

    IPs for local network in San Biagio

--- lscache4.sh
+++ lscache4.sh
@@ -1,6 +1,6 @@
 export COMMAND='cd /home/pi/basil ; ls -l cache'
 
-for i in 10 12 14 15
+for i in 46 47 48 49
 do
     echo "$i"
     ssh pi@192.168.1.$i $COMMAND
--- shutdown4.sh
+++ shutdown4.sh
@@ -1,6 +1,6 @@
 export COMMAND='sudo /sbin/shutdown now'
 
-for i in 10 12 14 15
+for i in 46 47 48 49
 do
     echo "$i"
     ssh pi@192.168.1.$i $COMMAND
