ce8d83fbd58b84b0876ce34e596963df4ba51218
Alessandro Valli <av@oblong.com>
2018-01-22 14:14:10 +0100

    palette bieta

--- bieta.py
+++ bieta.py
@@ -2,6 +2,13 @@ from vision import *
 
 def RoutineBieta(image_file, bgr, box):
     bgr = CropImage(bgr, cropname='bieta')
-    UpdateWindow('bgr', bgr)
+
+    bgr = Resize(bgr, 0.99)
     hsv = ToHSV(bgr)
-    UpdateWindow('hsv', hsv)
+
+    # EnablePaletteCreator(bgr, hsv, bins=24)
+
+    mask = SegmentGoodPalette(hsv, 'palette-bieta.pkl', 14.0, debug=False)
+    foreground = MaskedImage(bgr, mask)
+    UpdateWindow('foreground', foreground)
+    UpdateWindow('background', bgr - foreground)
