97d9fd967d9e8f2e434c22a41a9c99436005cb50
Alessandro Valli <av@oblong.com>
2018-01-22 14:32:17 +0100

    batavia

--- bataviarossa.py
+++ bataviarossa.py
@@ -2,6 +2,13 @@ from vision import *
 
 def RoutineBataviaRossa(image_file, bgr, box):
     bgr = CropImage(bgr, cropname='bataviarossa')
-    UpdateWindow('bgr', bgr)
+    
+    bgr = Resize(bgr, 0.99)
     hsv = ToHSV(bgr)
-    UpdateWindow('hsv', hsv)
+
+    # PurgePalette('palette-bataviarossa.pkl', 11)
+    # EnablePaletteCreator(bgr, hsv, bins=24)
+    mask = SegmentGoodPalette(hsv, 'palette-bataviarossa.pkl', 14.0, debug=False)
+    foreground = MaskedImage(bgr, mask)
+    UpdateWindow('foreground', foreground)
+    UpdateWindow('background', bgr - foreground)
