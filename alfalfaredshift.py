2b33fa3de91fde8ea19d8b164a721a219da47625
Alessandro Valli <av@oblong.com>
2018-01-22 01:39:57 +0100

    Canny

--- alfalfaredshift.py
+++ alfalfaredshift.py
@@ -1,7 +1,27 @@
 from vision import *
+from skimage import feature
+
+def auto_canny(image, sigma=0.33):
+    # compute the median of the single channel pixel intensities
+    v = np.median(image)
+    # apply automatic Canny edge detection using the computed median
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    print(lower,upper)
+    edged = cv2.Canny(image, lower, upper)
+    # return the edged image
+    return edged
 
 def RoutineAlfalfaRedshift(image_file, bgr, box):
     bgr = CropImage(bgr, cropname='redshift')
     UpdateWindow('bgr', bgr, image_file.replace('downloaded/', 'temp/') + '.jpeg')
     hsv = ToHSV(bgr)
     UpdateWindow('hsv', hsv)
+
+    edges = np.uint8(feature.canny(BGRToGray(bgr), sigma=2.0, low_threshold=20, high_threshold=50, use_quantiles=False)) * 255
+    UpdateWindow('canny1', edges)
+
+    # edged = auto_canny(bgr)
+    edged = cv2.Canny(bgr, 100, 200)
+    UpdateWindow('canny2', edged)
+
