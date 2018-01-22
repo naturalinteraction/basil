2117649f1f489fc67f03f311a7e13acabfb8bb02
Alessandro Valli <av@oblong.com>
2018-01-22 14:35:56 +0100

    rucola

--- rucola.py
+++ rucola.py
@@ -2,6 +2,13 @@ from vision import *
 
 def RoutineRucola(image_file, bgr, box):
     bgr = CropImage(bgr, cropname='rucola')
-    UpdateWindow('bgr', bgr)
+    bgr = Resize(bgr, 0.99)
     hsv = ToHSV(bgr)
-    UpdateWindow('hsv', hsv)
+
+    # PurgePalette('palette-.pkl', 11)
+    # EnablePaletteCreator(bgr, hsv, bins=24)
+
+    mask = SegmentGoodPalette(hsv, 'palette-rucola.pkl', 14.0, debug=False)
+    foreground = MaskedImage(bgr, mask)
+    UpdateWindow('foreground', foreground)
+    UpdateWindow('background', bgr - foreground)
