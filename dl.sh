c82dcca3cf79457f6dc09de32a258990f34a5995
Alessandro Valli <av@oblong.com>
2018-01-23 14:47:10 +0100

    download xxx

--- dl.sh
+++ dl.sh
@@ -1,6 +1,6 @@
-./go.sh -p redshift-sanb -s '' -d
-./go.sh -p blueshift-sanb -s '' -d
-./go.sh -p noir-sanb -s '' -d
-./go.sh -p visible-sanb -s '' -d
+./go.sh -p redshift-xxx -s '' -d
+./go.sh -p blueshift-xxx -s '' -d
+./go.sh -p noir-xxx -s '' -d
+./go.sh -p visible-xxx -s '' -d
 
 
