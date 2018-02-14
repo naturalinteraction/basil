3b0061aacccb18c3f2b93e91f0f8f858f479d5e5
Alessandro Valli <av@oblong.com>
2018-02-14 18:10:17 +0100

    saturation is satisfactory

--- rucola.py
+++ rucola.py
@@ -5,6 +5,7 @@ h = []
 s = []
 v = []
 sat_mean = []
+topped_sat_mean = []
 jitter = []
 tone_filename = 'rucola.temp'
 
@@ -37,26 +38,37 @@ def RoutineRucola(image_file, bgr, box):
     foreground = cv2.multiply(GrayToBGR(dist), bgr, scale=1.0/255.0)
     UpdateWindow('background', cv2.multiply(GrayToBGR(255 - dist), bgr, scale=1.0/255.0))
 
+    curve_alpha = 0.05
+
     saturation = cv2.split(hsv)[1]
     Normalize(saturation)
     UpdateWindow('normalized saturation', saturation)
-    ####ret,saturation = cv2.threshold(saturation, reference - trunc_sigmas * sigma, reference - trunc_sigmas * sigma, cv2.THRESH_TRUNC)
     sm = cv2.mean(saturation)[0]
     if len(sat_mean) > 0:
-        sat_mean.append(sm * 0.1 + 0.9 * sat_mean[-1])
+        sat_mean.append(sm * curve_alpha + (1.0 - curve_alpha) * sat_mean[-1])
     else:
         sat_mean.append(sm)
+    ret,saturation = cv2.threshold(saturation, 170, 170, cv2.THRESH_TRUNC)
+    Normalize(saturation)
+    UpdateWindow('topped normalized saturation', saturation)
+    sm = cv2.mean(saturation)[0]
+    if len(topped_sat_mean) > 0:
+        topped_sat_mean.append(sm * curve_alpha + (1.0 - curve_alpha) * topped_sat_mean[-1])
+    else:
+        topped_sat_mean.append(sm)
 
-    Echo(foreground, 'biomass p-index %.1f' % (AppendMeasurementJitter(dist, measurements, jitter, alpha=0.1)))
+    biomass = AppendMeasurementJitter(dist, measurements, jitter, alpha=0.1)
+    # Echo(foreground, 'biomass p-index %.1f' % (biomass))
 
     h.append(read_mean[0])
     s.append(read_mean[1])
     v.append(read_mean[2])
 
-    UpdateToneStats(dist, hsv, read_mean, read_std, tone_filename, alpha=0.1)
+    UpdateToneStats(dist, hsv, read_mean, read_std, tone_filename, alpha=curve_alpha)
 
     DrawChart(foreground, measurements)
     DrawChart(foreground, sat_mean, color=(0, 255, 255))
+    DrawChart(foreground, topped_sat_mean, color=(255, 255, 0))
 
     DrawChart(foreground, h, color=(255, 0, 0), xmult=10, xoffset=150, ymult=6, yoffset=500)
     DrawChart(foreground, s, color=(0, 255, 0), xmult=10, xoffset=150, ymult=6, yoffset=500)
