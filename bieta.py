1283d35ea54552447a45e8e43060467ee9f1706d
Alessandro Valli <av@oblong.com>
2018-02-20 18:10:40 +0100

    motion detection test

--- bieta.py
+++ bieta.py
@@ -3,36 +3,19 @@ from vision import *
 measurements = []
 
 def RoutineBieta(image_file, bgr, box):
-    bgr = CropImage(bgr, cropname='bieta')
-
-    # bgr = Resize(bgr, 0.99)
-    hsv = ToHSV(bgr)
-
-    # EnablePaletteCreator(bgr, hsv, bins=24)
-
-    mask = SegmentGoodPalette(hsv, 'palette-bieta.pkl', 14.0, debug=False)
-    foreground = MaskedImage(bgr, mask)
-    UpdateWindow('foreground', foreground)
-    UpdateWindow('background', bgr - foreground)
-
-    foreground = cv2.addWeighted(foreground, 0.6, bgr, 0.4, 0)
-
-    M = cv2.moments(mask)
-    # cy = (M['m01']/M['m00'])
-    biomass = (M['m00'] / 2000000)
-    Echo(foreground, 'biomass area ' + str(int(biomass)))
-    biomass = biomass - 40
-    if len(measurements) > 0:
-        biomass = 0.5 * biomass + 0.5 * measurements[-1]
-    measurements.append(biomass)
-
-    h,w = hsv.shape[:2]
-    for i in range(1, len(measurements)):
-        last = measurements[i]
-        previous = measurements[i - 1]
-        cv2.line(foreground, ((i - 1) * 10 + 50, int(h - previous * 9)), (i * 10 + 50, int(h - last * 9)), (255, 255, 255), 3)
-    # mean, std = ComputeStatsOfMaskedImage(hsv, mask)
-    # print(mean)
-
-    UpdateWindow('foreground', foreground, image_file.replace('downloaded/', 'temp/') + '.jpeg')
+    print(image_file.replace('.jpg', '').replace('downloaded/', '').replace('_', '-').split('-'))
+    # print(image_file)
     
+    bgr,hsv = ResizeBlur(bgr, 0.5, 33)
+
+    UpdateWindow('bgr', bgr)
+
+    global previous
+    try:
+        previous
+    except:
+        previous = bgr
+    motion = cv2.absdiff(bgr, previous)
+    UpdateWindow('motion', motion)
+    print(int(cv2.mean(BGRToGray(motion))[0]))
+    previous = bgr
