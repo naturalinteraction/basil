e9320d2a109063803ef7e478f4d75c5d0f09b790
Alessandro Valli <av@oblong.com>
2018-02-20 18:50:53 +0100

    more motion detection

--- bieta.py
+++ bieta.py
@@ -6,7 +6,7 @@ def RoutineBieta(image_file, bgr, box):
     print(image_file.replace('.jpg', '').replace('downloaded/', '').replace('_', '-').split('-'))
     # print(image_file)
     
-    bgr,hsv = ResizeBlur(bgr, 0.5, 33)
+    bgr,hsv = ResizeBlur(bgr, 0.2, 11)
 
     UpdateWindow('bgr', bgr)
 
@@ -16,6 +16,8 @@ def RoutineBieta(image_file, bgr, box):
     except:
         previous = bgr
     motion = cv2.absdiff(bgr, previous)
+    motion = BGRToGray(motion)
+    ret,motion = cv2.threshold(motion, 20, 20, cv2.THRESH_TOZERO)
     UpdateWindow('motion', motion)
-    print(int(cv2.mean(BGRToGray(motion))[0]))
+    print(int(cv2.mean(motion)[0]))
     previous = bgr
