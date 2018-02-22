71bc9c74065fac8fa81f6f5fe1f1349c0d3dc537
Alessandro Valli <av@oblong.com>
2018-02-22 12:26:23 +0100

    campaign is over on visible and blueshift

--- dl.sh
+++ dl.sh
@@ -1,4 +1,4 @@
 ./go.sh -p redshift-doublecalib -s '' -d
-./go.sh -p blueshift-doublecalib -s '' -d
+#./go.sh -p blueshift-doublecalib -s '' -d
 ./go.sh -p noir-doublecalib -s '' -d
-./go.sh -p visible-doublecalib -s '' -d
+#./go.sh -p visible-doublecalib -s '' -d
