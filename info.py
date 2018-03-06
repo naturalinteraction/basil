0ae6e7315b2a3ddfc88b57f2ec0cfb3bc40bad58
Alessandro Valli <av@naturalinteraction.org>
2018-03-06 15:32:24 +0100

    more verbosity in info.py

--- info.py
+++ info.py
@@ -2,10 +2,10 @@
 
 from utility import *
 
-print(GetMAC('eth0'))
-print(GetMAC('wlan0'))
-print(GetMAC('usb0'))
-print(GetCPUSerial())
-print(GetSDSerial())
-print(GetSDCID())
+print('eth0', GetMAC('eth0'))
+print('wlan0', GetMAC('wlan0'))
+print('usb0', GetMAC('usb0'))
+print('cpuserial', GetCPUSerial())
+print('sdserial', GetSDSerial())
+print('sdcid', GetSDCID())
 
