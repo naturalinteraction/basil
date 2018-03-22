e290fb0b0f157983d15d64058515dbbb35aa12ce
Alessandro Valli <av@naturalinteraction.org>
2018-03-22 11:48:32 +0100

    test mqtt with test.txt

--- cap.py
+++ cap.py
@@ -27,7 +27,7 @@ d['batchId'] = "valliBatchBig"
 d['lineId'] = 2
 d['fake'] = 420
 d['type'] = "image"
-UploadMQTT("zero/test/images", 'downloaded/test-test_2560x1920_2000_01_01-00_00.jpg', d)
+UploadMQTT("zero/test/images", 'test.txt', d)
 # end of temporary mqtt test
 
 def SaveLastPictureTicks(ticks, filename):
