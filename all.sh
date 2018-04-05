d39de26a04f24b19ce9cf9d4814dcc3b8eafc6a2
Alessandro Valli <av@oblong.com>
2018-04-05 11:59:15 +0200

    script to process series ceppi and hawk

--- all.sh
+++ all.sh
@@ -1,3 +1,4 @@
+:'
 ./go.sh -r curves -p redshift-sanbiagio1
 ./go.sh -r curves -p noir-sanbiagio1
 ./go.sh -r curves -p blueshift-sanbiagio1
@@ -11,3 +12,14 @@
 ./go.sh -r curves -p noir-doublecalib
 ./go.sh -r curves -p visible-doublecalib
 ./go.sh -r curves -p blueshift-doublecalib
+'
+
+./go.sh -r curves -p redshift-ceppi
+./go.sh -r curves -p noir-ceppi
+./go.sh -r curves -p visible-ceppi
+./go.sh -r curves -p blueshift-ceppi
+
+./go.sh -r curves -p redshift-hawk
+./go.sh -r curves -p noir-hawk
+./go.sh -r curves -p visible-hawk
+./go.sh -r curves -p blueshift-hawk
