27e45f6eeb071af710ee78f639738e8fdff13e0b
Alessandro Valli <av@oblong.com>
2018-04-17 21:05:17 +0200

    ignore changes to series.pkl

--- list4.sh
+++ list4.sh
@@ -1,4 +1,4 @@
-export COMMAND='hostname ; pgrep -x -f "python cap.py" -a ; vcgencmd measure_temp ; cd /home/pi/basil ; git update-index --assume-unchanged customer.txt ; echo 'zero' > customer.txt'
+export COMMAND='hostname ; pgrep -x -f "python cap.py" -a ; vcgencmd measure_temp ; cd /home/pi/basil ; git update-index --assume-unchanged series.pkl ; git update-index --assume-unchanged customer.txt ; echo 'zero' > customer.txt'
 
 for i in 2226 2227 2228 2229
 do
