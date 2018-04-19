a8fe5b275b537fc6582ad43663d66a9b34dbd740
Alessandro Valli <av@oblong.com>
2018-04-19 18:08:40 +0200

    biomass webchart

---
+++ webchart/convert.py
@@ -0,0 +1,17 @@
+import pickle
+
+def LoadTimeSeries(filename):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+def ConvertPickleToCSV(name):
+    x,y = LoadTimeSeries(name + '.pkl')
+    file = open(name + '.csv', 'w')
+    file.write('minutes,' + name + '\n')
+    for i in range(len(x)):
+        file.write(str(x[i]) + ',' + str(y[i]) + '\n')
+    file.close()
+
+ConvertPickleToCSV('biomass')
+ConvertPickleToCSV('motion')
+
