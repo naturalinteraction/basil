814cab7a515ababac6e84589168ec0ed5cc7a015
Alessandro Valli <av@oblong.com>
2018-04-21 08:40:17 +0200

    fill motion bars

--- website/server.sh
+++ website/server.sh
@@ -1,3 +1,3 @@
-# google-chrome http://localhost:8000/chart.html?csv=visible-hawk.csv &
-google-chrome http://localhost:8000/chart.html?csv=http://natural-interaction.s3-website-eu-west-1.amazonaws.com/zero/visible-hawk.csv &
+google-chrome http://localhost:8000/chart.html?csv=visible-hawk.csv &
+# google-chrome http://localhost:8000/chart.html?csv=http://natural-interaction.s3-website-eu-west-1.amazonaws.com/zero/visible-hawk.csv &
 python -m SimpleHTTPServer
