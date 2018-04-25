4f632d6f92384e8c0dcb3d21dfbf5b9d084f7fe4
Alessandro Valli <av@oblong.com>
2018-04-25 17:23:20 +0200

    list customers

--- website/server.sh
+++ website/server.sh
@@ -1,7 +1,7 @@
-# google-chrome http://localhost:8000/list_CSV.html &
-google-chrome http://localhost:8000/chart.html?csv=CSV/blueshift-aprile.csv &
-google-chrome http://localhost:8000/chart.html?csv=CSV/noir-aprile.csv &
-google-chrome http://localhost:8000/chart.html?csv=http://natural-interaction.s3-website-eu-west-1.amazonaws.com/CSV/zero/noir.csv &
+google-chrome http://localhost:8000/customers.html &
+# google-chrome http://localhost:8000/chart.html?csv=CSV/blueshift-aprile.csv &
+# google-chrome http://localhost:8000/chart.html?csv=CSV/noir-aprile.csv &
+# google-chrome http://localhost:8000/chart.html?csv=http://natural-interaction.s3-website-eu-west-1.amazonaws.com/CSV/zero/noir.csv &
 # google-chrome http://localhost:8000/chart.html?csv=CSV/visible-aprile.csv &
 # google-chrome http://localhost:8000/chart.html?csv=http://natural-interaction.s3-website-eu-west-1.amazonaws.com/CSV/zero/visible.csv &
 python -m SimpleHTTPServer
