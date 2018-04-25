9d6230a5663a64d268678c9acc6b1660150f9867
Alessandro Valli <av@oblong.com>
2018-04-25 17:46:13 +0200

    list all batches for a given group/customer

--- website/server.sh
+++ website/server.sh
@@ -1,4 +1,5 @@
-google-chrome http://localhost:8000/customers.html &
+google-chrome http://localhost:8000/batches.html?group=zero &
+# google-chrome http://localhost:8000/customers.html &
 # google-chrome http://localhost:8000/chart.html?csv=CSV/blueshift-aprile.csv &
 # google-chrome http://localhost:8000/chart.html?csv=CSV/noir-aprile.csv &
 # google-chrome http://localhost:8000/chart.html?csv=http://natural-interaction.s3-website-eu-west-1.amazonaws.com/CSV/zero/noir.csv &
